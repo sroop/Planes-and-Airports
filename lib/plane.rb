991dd4b3f18373bf3fc72da78ae263b6f726ae6a
Sroop <sroopsunar@hotmail.com>
2014-04-21 19:10:57 +0100

    Plane: raise errors for dumb commands

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,10 +9,12 @@ class Plane
 	end
 
 	def land
+		raise "Plane has already landed, idiot!" if !@flying
 		@flying = false
 	end
 
 	def take_off
+		raise "Plane is already flying, idiot!" if @flying
 		@flying = true
 	end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -14,7 +14,7 @@ describe "Plane" do
 		end
 	end
 
-	context "landing and taking off:" do
+	context "Landing and Take-off:" do
 		
 		it 'can land' do
 			boeing = Plane.new
@@ -27,6 +27,23 @@ describe "Plane" do
 			boeing.land
 			boeing.take_off
 			expect(boeing).to be_flying
+		end
+
+	end
+
+	context "Raise errors for idiotic commands:" do
+		
+		it 'cannot land when it is already not flying' do
+			boeing = Plane.new
+			boeing.land
+			expect(boeing).to_not be_flying
+			expect{ (boeing.land) }.to raise_error(RuntimeError)
+		end
+
+		it 'cannot take-off when it is already flying' do
+			boeing = Plane.new
+			expect(boeing).to be_flying
+			expect { (boeing.take_off) }.to raise_error(RuntimeError)			
 		end
 
 	end
